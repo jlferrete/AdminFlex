a52ca1fc598e2434c5f67a5beadab37f6c98c410
JlFerrete <joseluisferrete@gmail.com>
2019-12-17 19:20:17 +0100

    Fixed npm install issue

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,21 @@ function css() {
     return gulp
         .src('scss/app.scss')
         .pipe(autoprefixer({
-            overrideBrowserslist : ['last 2 versions'],
+            overrideBrowserslist: ['last 2 versions'],
             cascade: false
         }))
         .pipe(sass({
-            outputStyle: 'expanded', // nested, compact, compressed
+            outputStyle: 'expanded', //nested, compact, compressed
         }))
-        .pipe( gulp.dest('css') );
+        .pipe(gulp.dest('css'));
 }
 
 function watchFiles() {
-    gulp.watch('scss/*.scss', css); 
+    gulp.watch('scss/*.scss', css);
     gulp.watch('index.html');
 }
 
+//Registrar funciones como tareas
 
-// Registrar funciones como tareas
-gulp.task( 'css', css );
-gulp.task('watch', gulp.parallel( watchFiles) );
+gulp.task('css', css);
+gulp.task('watch', gulp.parallel(watchFiles));
